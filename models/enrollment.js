f48d91856949bce9075450d220c98f6eddb984dd
Ushantha <ushanthamr@gmail.com>
2025-11-03 20:02:48 +0530

    Enrollment Models created.

---
+++ models/enrollment.js
@@ -0,0 +1,25 @@
+// models/Enrollment.js
+import mongoose from "mongoose";
+
+const enrollmentSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    progress: { type: Number, default: 0 }, // in %
+    status: { type: String, enum: ["active", "completed"], default: "active" },
+    enrolledAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+
+enrollmentSchema.index({ student: 1, course: 1 }, { unique: true }); // prevent duplicate enrollment
+
+export default mongoose.model("Enrollment", enrollmentSchema);
