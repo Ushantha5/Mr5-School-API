5170266703bec9f69cc18ea14b073c8567626fd6
Ushantha <ushanthamr@gmail.com>
2025-11-03 20:17:51 +0530

    Add payment routes for handling payment operations.

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getAllPayments,
+  getPaymentById,
+  createPayment,
+  updatePayment,
+  deletePayment,
+} from "../controllers/itemController.js";
+const router = express.Router();
+router.get("/", getAllPayments);
+router.get("/:id", getPaymentById);
+router.post("/", createPayment);
+router.put("/:id", updatePayment);
+router.delete("/:id", deletePayment);
+
+export default router;
