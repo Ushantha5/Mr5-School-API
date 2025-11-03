92c2c0e7596a8161ccf0a0797338beb5ef6a116e
Ushantha <ushanthamr@gmail.com>
2025-11-03 20:24:15 +0530

    Add assignment routes for CRUD operations.

---
+++ routes/assignmentRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getAllAssignments,
+  getAssignmentById,
+  createAssignment,
+  updateAssignment,
+  deleteAssignment,
+} from "../controllers/itemController.js";
+const router = express.Router();
+router.get("/", getAllAssignments);
+router.get("/:id", getAssignmentById);
+router.post("/", createAssignment);
+router.put("/:id", updateAssignment);
+router.delete("/:id", deleteAssignment);
+
+export default router;
