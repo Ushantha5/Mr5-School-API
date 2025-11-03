517e31f4d8862c4df72b4c9535cf54fe280b7bd6
Ushantha <ushanthamr@gmail.com>
2025-11-03 20:01:47 +0530

    Assignment models created .

---
+++ models/assignment.js
@@ -0,0 +1,23 @@
+// models/Assignment.js
+import mongoose from "mongoose";
+
+const assignmentSchema = new mongoose.Schema(
+  {
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    teacher: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true },
+    description: String,
+    dueDate: Date,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Assignment", assignmentSchema);
