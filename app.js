fdbc6f6f77a775a1f71921e8400383fd56b985c4
Ushantha <ushanthamr@gmail.com>
2025-11-04 09:55:55 +0530

    import same routes app.js.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import morgan from "morgan";
+// import cors from "cors";
+// import morgan from "morgan";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import submissionRoutes from "./routes/submissionRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import lessonRoutes from "./routes/lessonRoutes.js";
+import enrollmentRoutes from "./routes/enrollmentRoutes.js";
+import courseRoutes from "./routes/courseRoutes.js";
+import assignmentRoutes from "./routes/assignmentRoutes.js";
+import aI_Assistant_InterctionRoutes from "./routes/aI_Assistant_InterctionRoutes.js";
 
 dotenv.config();
 
@@ -11,16 +18,20 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
-app.use(morgan("dev"));
+// app.use(cors());
+// app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
-app.use("/api/items", itemRoutes);
-app.use("/api/sales", saleRoutes);
-app.use("/api/auth", authRoutes);
+app.use("/api/submissions", submissionRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/lessons", lessonRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/enrollments", enrollmentRoutes);
+app.use("/api/assignments", assignmentRoutes);
+app.use("/api/aI_Assistant_Interctions", aI_Assistant_InterctionRoutes);
 
-app.use(errorHandler);
+// app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
